12db9b2eb3b2267ba628cf20d2b10e7b520c16dc
SharifKhalid <sharifkhalid161097@gmail.com>
2022-04-18 23:36:17 +0600

    bonus

--- src/Components/Header/Blogs/Blogs.js
+++ src/Components/Header/Blogs/Blogs.js
@@ -3,7 +3,24 @@ import './Blogs.css'
 const Blogs = () => {
     return (
         <div>
-            <h1>this is bloger.</h1>
+            <div>
+                <h2> Authentication ও Authorization এর মধ্যে পার্থক্য</h2>
+
+                <p>Authentication হচ্ছে user কে verify করার একাটি প্রক্রিয়া। আর Authorization হচ্ছে user কোন কোন applications, files, data ইত্যাদি access করতে পারবে, সেটা verify করার একাটি প্রক্রিয়া।                     
+                </p>
+            </div>
+
+            <div>
+                <h2> </h2>
+
+                <p></p>
+            </div>
+
+            <div>
+                <h2> </h2>
+
+                <p></p>
+            </div>
         </div>
     );
 };
