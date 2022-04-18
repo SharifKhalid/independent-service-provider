e0521cf4f78f5529fa0e8b992b1357ec4c0626a5
SharifKhalid <sharifkhalid161097@gmail.com>
2022-04-18 23:25:09 +0600

    banner

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,15 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer'>
-            <h1>footer</h1>
+            <div>
+                <h5 className='footer-h5'>Terms and conditions</h5>
+            </div>
+            
+            <div>
+                <h5 className='footer-h5'>contacts</h5>
+                <h6 className='footer-h5'>Email: photograper@gmail.com</h6>
+                <h6 className='footer-h5'>Phone: 3438557740</h6>
+            </div>
         </div>
     );
 };
